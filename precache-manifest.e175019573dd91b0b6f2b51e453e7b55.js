fa5686d9610dc5c71640ab84806c527486065bd4
Damian Krawczyk <damiankrawczyk23@gmail.com>
2019-04-16 17:56:28 +0100

    Updates

---
+++ precache-manifest.e175019573dd91b0b6f2b51e453e7b55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4be6ba6669d7ba5e50d",
+    "url": "/react-router-spa-project/static/js/runtime~main.b4be6ba6.js"
+  },
+  {
+    "revision": "3708899b5b9f688e41f4",
+    "url": "/react-router-spa-project/static/js/main.3708899b.chunk.js"
+  },
+  {
+    "revision": "d88e074bae164e6cd406",
+    "url": "/react-router-spa-project/static/js/2.d88e074b.chunk.js"
+  },
+  {
+    "revision": "3708899b5b9f688e41f4",
+    "url": "/react-router-spa-project/static/css/main.ee29f236.chunk.css"
+  },
+  {
+    "revision": "35afc5bc139477cf9641f5f236577d8b",
+    "url": "/react-router-spa-project/index.html"
+  }
+];
